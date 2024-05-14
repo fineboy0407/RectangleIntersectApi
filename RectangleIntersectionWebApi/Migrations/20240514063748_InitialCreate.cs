3555f45ab7647f76bb1e9d60fd92fe90b5f50bcb
fineboy0407 <levilee.fine@gmail.com>
2024-05-14 01:44:12 -0500

    feat: initial migration

---
+++ RectangleIntersectionWebApi/Migrations/20240514063748_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RectangleIntersectionWebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rectangles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    X1 = table.Column<double>(type: "float", nullable: false),
+                    Y1 = table.Column<double>(type: "float", nullable: false),
+                    X2 = table.Column<double>(type: "float", nullable: false),
+                    Y2 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rectangles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rectangles");
+        }
+    }
+}
